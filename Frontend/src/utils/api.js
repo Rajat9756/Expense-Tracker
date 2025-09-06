cc3256be4807661251dcf10b30d4c4901af02653
Rajat9756 <rajatpal2021gaj@gmail.com>
2025-09-06 16:32:21 +0530

    Expense-Tracker

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://expense-tracker-xiqe.onrender.com/api", // your backend URL
+  baseURL: "https://expense-tracker-yox9.onrender.com/api", // your backend URL
 });
 
 export default api;
